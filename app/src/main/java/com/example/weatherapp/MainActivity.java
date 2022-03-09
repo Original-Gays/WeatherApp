c7f0a290cd421e867afc26915b8c861fef66df71
rezortiez <rezortiez@gmail.com>
2022-03-10 02:35:25 +0300

    did simple design

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,26 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.weatherapp.weather.CurrentWeatherGetter;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText enteredData;
-    private TextView dataPlace;
+    /*private EditText enteredData;
+    private TextView dataPlace;*/
+
+    private RelativeLayout RLHome;
+    private ImageView IVBack, IVSearchIcon, IVConditionIcon;
+    private TextView TVTemperature, TVCondition, TVCityName;
+    private TextInputEditText EDTCity;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
 
-        enteredData = findViewById(R.id.gettingData);
-        dataPlace = findViewById(R.id.data_place);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity_main);
+
+        RLHome = findViewById(R.id.idRLHome);
+        IVBack = findViewById(R.id.idIVBack);
+        IVSearchIcon = findViewById(R.id.idIVSearchIcon);
+        IVConditionIcon = findViewById(R.id.idIVConditionIcon);
+        TVTemperature = findViewById(R.id.idTVTemperature);
+        TVCondition = findViewById(R.id.idTVCondition);
+        TVCityName = findViewById(R.id.idTVCityName);
+        EDTCity = findViewById(R.id.idEDTCity);
+
+        IVSearchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWeather(view);
+            }
+        });
+
+        /*enteredData = findViewById(R.id.gettingData);
+        dataPlace = findViewById(R.id.data_place);*/
+
     }
 
     public void getWeather(View view) {
-        CurrentWeatherGetter.getCurrentWeatherMetric(enteredData.getText().toString(), dataPlace);
-        enteredData.setText("");
+        /*CurrentWeatherGetter.getCurrentWeatherMetric(EDTCity.getText().toString(), TVCityName);
+        EDTCity.setText("");*/
+        CurrentWeatherGetter.getCurrentWeatherMetric(EDTCity.getText().toString(), TVTemperature, TVCondition, TVCityName, IVConditionIcon, IVBack);
+        EDTCity.setText("");
     }
 }
--- app/src/main/java/com/example/weatherapp/weather/CurrentWeatherGetter.java
+++ app/src/main/java/com/example/weatherapp/weather/CurrentWeatherGetter.java
@@ -1,6 +1,12 @@
 package com.example.weatherapp.weather;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.weatherapp.R;
+import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,7 +28,7 @@ public class CurrentWeatherGetter {
         return currentWeatherAPI.getCurrentWeather(CurrentWeatherGetter.API, city);
     }
 
-    public static void getCurrentWeatherMetric(String city, TextView dataPlace) {
+    public static void getCurrentWeatherMetric(String city, TextView Temperature, TextView Condition, TextView CityName, ImageView CondIcon, ImageView Background) {
         Call<CurrentWeather> call = Parse(city);
         call.enqueue(new Callback<CurrentWeather>() {
             @Override
@@ -33,13 +39,28 @@ public class CurrentWeatherGetter {
                 CurrentWeather currentWeather = response.body();
                 Location location = currentWeather.getLocation();
                 Current current = currentWeather.getCurrent();
-                String res = location.getName() + ", " + location.getRegion() + ", " + location.getCountry() + "\n" +
+
+                CityName.setText(location.getName());
+                Temperature.setText(current.getTemp_c()+"°C");
+                Condition.setText(current.getCondition().getText());
+                Picasso.get().load("https:"+current.getCondition().getIcon()).into(CondIcon);
+
+                if (current.isIs_day() == 1)
+                {
+                    Picasso.get().load("https://images.unsplash.com/photo-1548670123-2d3bb97f4678?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80").into(Background);
+                }
+                else
+                {
+                    Picasso.get().load("https://images.unsplash.com/photo-1454177697940-c43d9f9a7307?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80").into(Background);
+                }
+
+                /*String res = location.getName() + ", " + location.getRegion() + ", " + location.getCountry() + "\n" +
                              location.getLocaltime() + "\n" +
                              "Temperature: " + current.getTemp_c() + ", " +
                              current.getCondition().getText() + "\n" +
                              "Feels like: " + current.getFeelslike_c() + "\n" +
                              "Wind: " + current.getWind_kph();
-                dataPlace.setText(res);
+                dataPlace.setText(res);*/
             }
 
             @Override
