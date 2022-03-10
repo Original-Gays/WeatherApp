13aa468177a962cf50902681d02c7302406d9187
rezortiez <rezortiez@gmail.com>
2022-03-10 08:36:43 +0300

    status bar fixed + minor design changes

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -22,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_activity_main);
